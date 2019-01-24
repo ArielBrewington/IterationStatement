d10ecb2e6dd662c454cb617e8d44781dcc460a30
Ariel Brewington <ArielBrewington@outlook.com>
2019-01-24 16:02:06 -0500

    Iterationstatement

--- IterationStatement/Program.cs
+++ IterationStatement/Program.cs
@@ -6,7 +6,38 @@ namespace IterationStatement
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter an integer between 1 and 100");
+
+            try
+            {
+
+                string Input = Console.ReadLine();
+                int valueofinput = int.Parse(Input);
+
+
+                if ((valueofinput > 0) && (valueofinput < 10))
+                    for (int i = 0; i < valueofinput; i++)
+                    {
+                        Console.WriteLine("You have entered" + valueofinput.ToString() + "This is the current integer value in the loop:" + i.ToString());
+                    }
+                {
+
+                    Console.WriteLine("Press any key to exit the program");
+                    Console.ReadKey(true);
+                }
+                {
+                }
+            }
+
+
+
+
+            catch
+            {
+            }
+
+
+        
         }
     }
-}
+} 
